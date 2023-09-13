cdaadfe44ab956558a36131787dbde033f458962
De1dMonster <144820063+De1dMonster@users.noreply.github.com>
2023-09-13 20:43:29 +0530

    Update logs.py

--- AltBots/modules/logs.py
+++ AltBots/modules/logs.py
@@ -42,17 +42,21 @@ async def logs(legend):
         fetch = await legend.reply(f"__Fetching Logs...__")
     
         with open("AltLogs.txt", "w") as logfile:
-            logfile.write("⚡ CODEX ⚡ [ Bot Logs ]\n\n" + logs)
+            logfile.write("⚡ ☆ 𐎓⃝🌺🇲𝗼𝗻𝘀𝘁𝗲𝗿❤‍🔥⃟⃚⃐ 🌿  ☆ 
+
+ ➪ 𝗣ɨɳɠ: 342.2 ᴍs 
+ ➪ 𝗨թƬɨмє: 12m:16s 
+ ➪ ⩔єяនɨ០ɳ: v0.6⚡ [ Bot Logs ]\n\n" + logs)
 
         end = datetime.now()
         ms = (end-start).seconds
         await asyncio.sleep(1)
 
         try:
-            await X1.send_file(legend.chat_id, "AltLogs.txt", caption=f"⚡ **CODEX LOGS** ⚡\n  » **ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:** `{ms} ꜱᴇᴄᴏɴᴅꜱ`")
+            await X1.send_file(legend.chat_id, "AltLogs.txt", caption=f"⚡ **MONSTERX LOGS** ⚡\n  » **ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:** `{ms} ꜱᴇᴄᴏɴᴅꜱ`")
             await fetch.delete()
         except Exception as e:
             await fetch.edit(f"An Exception Occured!\n\n**ERROR:** {str(e)}")
 
     elif legend.sender_id in SUDO_USERS:
-        await legend.reply("» ⚡𝗖𝗢𝗗𝗘𝗫 𝙲𝙰𝙽 𝙰𝙲𝙴𝚂𝚂  𝙲𝙾𝙼𝙼𝙰𝙽𝙳⚡.")
+        await legend.reply("» ⚡𝗠𝗢𝗡𝗦𝗧𝗘𝗥 𝙲𝙰𝙽 𝙰𝙲𝙴𝚂𝚂  𝙲𝙾𝙼𝙼𝙰𝙽𝙳⚡.")
