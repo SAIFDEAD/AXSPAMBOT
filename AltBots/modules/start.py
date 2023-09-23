750f9f5406fe28db6eaaa77ed70a42926b865a6b
De1dMonster <144820063+De1dMonster@users.noreply.github.com>
2023-09-23 23:19:54 +0530

    Update start.py

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -1,18 +1,18 @@
-from telethon import version, events, Button
+from telethon import __version__, events, Button
 
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 
 START_BUTTON = [
     [
-        Button.inline("🥃 cσɱɱαɳ∂ 🥃", data="help_back")
+        Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
     ],
     [
-        Button.url("🥃 οωиєя 🥃", "https://t.me/DAEDX_HELP"),
-        Button.url("🥃 ѕυρροяτ 🥃", "https://t.me/Saif_Dictator")
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/TheAltron"),
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/AltronChats")
     ],
     [
-        Button.url("🥃 Dεѵεℓσρεɾ 🥃", "https://t.me/DAEDX_HELP")
+        Button.url("• ʀᴇᴘᴏ •", "https://github.com/ItZxSTaR/XBOTS")
     ]
 ]
 
@@ -33,14 +33,14 @@ async def start(event):
         AltBot = await event.client.get_me()
         bot_name = AltBot.first_name
         bot_id = AltBot.id
-        TEXT = f"ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [CODEX](https://t.me/DAEDX_HELP)\n\n"
-        TEXT += f"» xʙᴏᴛꜱ ᴠᴇʀsɪᴏɴ : M3.3\n"
-        TEXT += f"» ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : 3.11.3\n"
-        TEXT += f"» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : {version}\n━━━━━━━━━━━━━━━━━"
+        TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝐀xᴇɴ](https://t.me/PyXen)**\n\n"
+        TEXT += f"» **xʙᴏᴛꜱ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
+        TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
+        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
                     event.chat_id,
-                    "https://graph.org/file/f4e84fef88c1c848c138f.jpg",
+                    "https://te.legra.ph/file/07d39b85c6cea32f15259.jpg",
                     caption=TEXT, 
                     buttons=START_BUTTON
 )
