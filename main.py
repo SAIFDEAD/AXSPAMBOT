d2826744270ba839be9bf8f8c585f9eb312cba3c
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2023-11-07 01:16:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def load_plugins(plugin_name):
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
     sys.modules["AltBots.modules." + plugin_name] = load
-    print("Altron has Imported " + plugin_name)
+    print("Dead Spam  has Imported " + plugin_name)
 
 
 files = glob.glob("AltBots/modules/*.py")
